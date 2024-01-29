744969cfb9e74d03cca164c521b0f72bf5eae87c
Lucas Lima <lucaserolima@gmail.com>
2024-01-29 18:17:59 -0400

    Mapping the URI for sending messages

--- src/main/java/br/com/example/chat/controller/App.java
+++ src/main/java/br/com/example/chat/controller/App.java
@@ -1,10 +1,15 @@
 package br.com.example.chat.controller;
 
 import br.com.example.chat.config.Msg;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class App {
+
+    @MessageMapping("/chatmessage")
+    @SendTo("/chat")
     public Msg sendMessage(Msg message){
         return message;
     }
