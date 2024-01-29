972f0137017405255caf4b33509c2d6b182bb59a
Lucas Lima <lucaserolima@gmail.com>
2024-01-29 18:08:54 -0400

    Overriding and configuring the WebSocket channel and determining the chat channel prefix names

--- src/main/java/br/com/example/chat/config/WebSocketConfig.java
+++ src/main/java/br/com/example/chat/config/WebSocketConfig.java
@@ -1,6 +1,7 @@
 package br.com.example.chat.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -13,5 +14,11 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/connect");
         registry.addEndpoint("connect").withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/chat");
+        registry.setApplicationDestinationPrefixes("/app");
     }
 }
