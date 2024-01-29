6f7a8daeb31068487a8a58892ddd5c9a09822c80
Lucas Lima <lucaserolima@gmail.com>
2024-01-29 17:59:49 -0400

    Creating the 'Msg' class that will store the user and his message.

---
+++ src/main/java/br/com/example/chat/config/Msg.java
@@ -0,0 +1,15 @@
+package br.com.example.chat.config;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Msg {
+    private String user;
+    private String msg;
+}
